1fe053b39ac145522ccd34ce03d284e0dfde062e
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-02-02 13:31:48 +0900

    spring init

---
+++ src/main/java/com/example/winterdeom/WinterDeomApplication.java
@@ -0,0 +1,13 @@
+package com.example.winterdeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterDeomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterDeomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/winterdeom/WinterDeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winterdeom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterDeomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
