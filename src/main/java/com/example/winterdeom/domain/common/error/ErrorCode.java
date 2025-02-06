e72c8d8af339bee40079fe5f3b59652d2a0805cb
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-02-06 13:50:16 +0900

    feat: 불필요한 코드 제거 - #3

--- src/main/java/com/example/winterdeom/domain/common/error/ErrorCode.java
+++ src/main/java/com/example/winterdeom/domain/common/error/ErrorCode.java
@@ -38,8 +38,7 @@ public enum ErrorCode {
     NOT_NULL("9001", "필수값이 누락되었습니다."),
     NOT_BLANK("9001", "필수값이 빈 값이거나 공백으로 되어있습니다."),
     REGEX("9002", "형식에 맞지 않습니다. :"),
-    LENGTH("9003", "길이가 유효하지 않습니다. :"),
-    ASSERT_TRUE("9004", "약관에 동의해주세요.");
+    LENGTH("9003", "길이가 유효하지 않습니다. :")
 
     private final String code;
     private final String message;
