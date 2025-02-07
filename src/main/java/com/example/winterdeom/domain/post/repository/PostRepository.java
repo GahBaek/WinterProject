b4729f4c698c95b468af5a7c781135dffc262a1d
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-02-07 16:02:44 +0900

    feat: findByUser 추가 -#5

--- src/main/java/com/example/winterdeom/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/winterdeom/domain/post/repository/PostRepository.java
@@ -3,9 +3,12 @@ package com.example.winterdeom.domain.post.repository;
 import com.example.winterdeom.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
     Optional<Post> findByUserIdAndId(UUID userId, UUID postId);
+
+    Optional<Post> findByUserId();
 }
