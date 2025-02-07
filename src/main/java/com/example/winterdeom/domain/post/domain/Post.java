a1f7fe707a5184f07962b0a21e979d7bc5c390a4
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-02-07 11:13:17 +0900

    feat: Post entity 작성 - #5

--- src/main/java/com/example/winterdeom/domain/post/domain/Post.java
+++ src/main/java/com/example/winterdeom/domain/post/domain/Post.java
@@ -1,4 +1,20 @@
 package com.example.winterdeom.domain.post.domain;
 
-public class Post {
+import com.example.winterdeom.domain.common.BaseEntity;
+import com.example.winterdeom.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "post")
+public class Post extends BaseEntity {
+    private String title;
+    private String content;
+    private User user;
 }
