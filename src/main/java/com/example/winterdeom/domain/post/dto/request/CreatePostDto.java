928efdc165a0ec67c55c8a021e93fc82fb82a7ac
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-2.local>
2025-02-07 13:08:48 +0900

    feat: annotation 추가 - #5

--- src/main/java/com/example/winterdeom/domain/post/dto/request/CreatePostDto.java
+++ src/main/java/com/example/winterdeom/domain/post/dto/request/CreatePostDto.java
@@ -1,11 +1,17 @@
 package com.example.winterdeom.domain.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CreatePostDto {
-    private String title;
-    private String content;
+    @NotNull
+    String title;
+    @NotBlank
+    String content;
 
     public CreatePostDto(String title, String content) {
         this.title = title;
