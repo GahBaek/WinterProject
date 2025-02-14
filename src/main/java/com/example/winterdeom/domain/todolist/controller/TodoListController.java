0dd7d04753a09dee4c9ed87989ae878ff568832b
LeeJiSoo <ljs08010@gmail.com>
2025-02-14 22:29:46 +0900

    [CHORE] 패키지 변경

--- src/main/java/com/example/winterdeom/domain/todolist/controller/TodoListController.java
+++ src/main/java/com/example/winterdeom/domain/todolist/controller/TodoListController.java
@@ -4,11 +4,11 @@ import com.example.winterdeom.domain.todolist.dto.request.TodoListReq;
 import com.example.winterdeom.domain.todolist.service.TodoListService;
 import com.example.winterdeom.domain.user.domain.User;
 import com.example.winterdeom.global.auth.AuthenticatedUser;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class TodoListController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Map.of(
-                        "message", "투두리스트를 정상적으로 생성되었습니다.",
+                        "message", "투두리스트가 정상적으로 생성되었습니다.",
                         "todoListId", todoListId
                 ));
     }
